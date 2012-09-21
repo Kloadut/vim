e7f0d41ea0b8dbbd70cf8466a63afb384425baf5
Alexis Gavoty <agavoty@linagora.com>
2012-09-21 11:32:06 +0200

    tweaks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-endpath="$HOME/.spf13-vim-3"
+endpath="$HOME/.kload-vim"
 
 warn() {
     echo "$1" >&2
@@ -16,7 +16,7 @@ echo "thanks for installing spf13-vim\n"
 # Backup existing .vim stuff
 echo "backing up current vim config\n"
 today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.color.vim; do [ -e $i ] && mv $i $i.$today; done
 
 
 echo "cloning spf13-vim\n"
@@ -24,6 +24,7 @@ git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
 mkdir -p $endpath/.vim/bundle
 ln -s $endpath/.vimrc $HOME/.vimrc
 ln -s $endpath/.vim $HOME/.vim
+ln -s $endpath/.color.vim $HOME/.color.vim
 
 echo "Installing Vundle"
 git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
